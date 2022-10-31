6561b4ab5ec722c7e964f8fac39e7d2eb26d17ee
Varun Kumar <nccarmyvarun@gmail.com>
2022-10-31 22:32:22 +0530

    cart dropdown added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,45 @@
 import React from 'react'
+import { useState } from 'react'
+import './Header.css'
 function Header(props){
 
-   // console.log(props.data[1].cardData.Name);
+     const [display,setDisplay] = useState('none');
+
+     const HandleDisplay = () =>{
+       if(display === "none"){
+         setDisplay("block");
+       } else if(display === "block"){
+         setDisplay("none");
+       }
+
+     }
+
 
   return(
       <>
-      {
-        props.data.map((curr,currIndex)=>{
-            
-            return(
-               <>
-                   <div className='main' key={curr}>
-                        <p>Name : {curr.cardData.Name}</p>
-                        <h1>Price : {curr.cardData.price}</h1>
+ <div className='backdrop_navabar'>
+ <div className='navbar_right'><h1>E commerce App</h1></div>
 
-                   </div>
-               </>
-           )
-       })
+ <div className='navbar_left'> 
+ <i class="fa-solid fa-cart-shopping" onClick={HandleDisplay} ></i>
+      <div className='counter'>{props.data.length}</div>
+ 
+      {
+        props.data.map((curr,currIndex)=>{   
+        return(
+            <>
+      <div className='main_navbar' key={curr} style={{display:display}}>
+                <p>Name : {curr.cardData.Name}</p>
+                
+                <p>Price : {curr.cardData.price}</p>
+                <hr />
+                </div>
+            </>
+           )})
       }
+      </div>
+
+      </div>
 
       </>
   )
